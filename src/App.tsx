54f0a581f7f880fef175a978528f99312d162898
davifa1 <davihenriquefavaro@gmail.com>
2022-09-23 18:32:27 -0300

    `animation_frame_motion`

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
+import { motion } from 'framer-motion';
 import './main.css';
+
 import Card from './assets/Card.svg';
 import Pix from './assets/Pix.svg';
 import Barcode from './assets/Barcode.svg';
@@ -7,26 +9,72 @@ import Arrowup from './assets/Arrowup.svg';
 import Arrowdown from './assets/Arrowdown.svg';
 import Trandingup from './assets/Trendingup.svg';
 
+
 function App() {
+  const buttons = {
+    hidden: { opacity: 1, scale: 0.1},
+    visible: {
+      opacity: 1,
+      scale: 1,
+      transition: {
+        delayChildren: 1,
+        staggerChildren: 1
+      }
+    }
+  }
+
+    const Money = {
+      hidden: {y: 20, opacity: 0},
+      visible: {
+        y: 0,
+        opacity: 1
+      }
+    }
+
+    const City = {
+      hidden: {width: 0, scale: 0.1},
+      visible: {
+        width: 150,
+        scale: 1,
+        transition: {
+          delayChildren: 1,
+          staggerChildren: 1
+        }
+      }
+    }
+
 
   return (
     <div className="bg-[#f7f5f5] w-full">
       <div className='flex flex-row items-center justify-between mx-[2rem] w-[20rem]'>
         <p className='text-orange-600 font-black text-[3rem]'>Inter</p>
 
-        <div className='border border-solid border-black rounded-full w-[10rem] justify-between flex'>
+        <motion.div
+        variants={City}
+        initial="hidden"
+        animate="visible"
+        className='border border-solid border-black rounded-full w-[10rem] justify-between flex'>
           <button className='mx-3'>Brasil</button>
           <button className='mx-3'>USA</button>
-        </div>
+        </motion.div>
       </div>
 
-      <div className='mx-[2rem] '>
+      <motion.div 
+      variants={Money}
+      initial="hidden"
+      animate="visible"
+
+      className='mx-[2rem] '>
         <h1 className='text-[40px] font-semibold'>R$: 10.000</h1>
         <button className='text-orange-700 font-semibold'>Ver Extrato</button>
-      </div>
+      </motion.div>
 
       <div className='flex flex-col my-10 justify-center items-center bg-[#e9e9e9]'>
-        <div className='grid grid-cols-3 gap-10'>
+        <motion.div
+          variants={buttons}
+          initial="hidden"
+          animate="visible"
+          className='grid grid-cols-3 gap-10 mt-[-5px] pb-20'>
           <button className='border border-solid border-black rounded w-40 h-28 bg-white'>
             <div className='items-center justify-center flex'>
               <img src={Card} className='w-10' />
@@ -35,57 +83,33 @@ function App() {
             Cartao
           </button>
 
-          <button className='border border-solid border-black rounded w-40 h-28'>
+          <button className='border border-solid border-black rounded w-40 h-28 bg-white'>
             <div className='items-center justify-center flex'>
               <img src={Pix} className='w-10' />
             </div>
             Pix
           </button>
 
-          <button className='border border-solid border-black rounded w-40 h-28'>
+          <button className='border border-solid border-black rounded w-40 h-28 bg-white'>
             <div className='items-center justify-center flex'>
               <img src={Trandingup} className='w-10' />
             </div>
             Investimentos
           </button>
 
-          <button className='border border-solid border-black rounded w-40 h-28'>
-          <div className='items-center justify-center flex'>
-              <img src={Barcode} className='w-10' />
-            </div>
-            Pagamentos
-          </button>
 
-          <button className='border border-solid border-black rounded w-40 h-28'>
-          <div className='items-center justify-center flex'>
-              <img src={Money} className='w-10' />
-            </div>
-            Transferir
-          </button>
-
-          <button className='border border-solid border-black rounded w-40 h-28'>
-          <div className='items-center justify-center flex'>
-              <img src={Trandingup} className='w-10' />
-            </div>
-            Cartao
-          </button>
-        </div>
-
-        <div className='w-full h-6 items-center justify-center flex my-7'>
-          <button>
-          <img src={Arrowdown} className='w-20' />
-          </button>
-        </div>
+        </motion.div>
       </div>
 
       <div className='flex flex-col  w-full'>
         <div className='items-center justify-between flex w-ful mx-[5rem] mb-5'>
-        <h1 className='text-[1.6rem] text-black font-medium'>Ofertas Pra você</h1>
-        <button className='text-[1.6rem] text-orange-600'>X</button>
+          <h1 className='text-[1.4rem] text-black font-medium'>Ofertas Pra você</h1>
         </div>
 
         <div className='flex items-center justify-center'>
-          <img src='https://marketplace.bancointer.com.br/banners/b8fe8e67-ee02-4426-bf4d-652a1ad30cc4' />
+          <img src='https://marketplace.bancointer.com.br/banners/a7748730-4c86-45b0-bc91-795cd4320b2b' 
+          className='w-[20cm]'
+          />
         </div>
       </div>
     </div >
